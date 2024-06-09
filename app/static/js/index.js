d0e4c7d6ac40fde9af8747261961f28253ee075d
Huard-Lefebvre, Antonin <antoninhuard.lefebvre@gmail.com>
2024-06-09 11:11:19 -0400

    Debute l'interface web

---
+++ app/static/js/index.js
@@ -0,0 +1,8 @@
+document.getElementById("buttontest").addEventListener("click", cliquerSurLeBoutonSuccess);
+
+function cliquerSurLeBoutonSuccess() {
+    let ul = document.createElement('ul');
+        ul.setAttribute('id','proList');
+
+        productList = ['Ingredient1','Ingredient2','Ingredient3','Ingredient4'];
+} 

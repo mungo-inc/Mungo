1863b5347ba747f8c54108393f6662d69fa707a0
Huard-Lefebvre, Antonin <antoninhuard.lefebvre@gmail.com>
2024-06-16 20:23:16 -0400

    Refactore avoir_recettes()

--- app/index.py
+++ app/index.py
@@ -28,9 +28,8 @@ def search():
     allergies = request.args.getlist('allergie')
     dietes = request.args.getlist('diete')
     budget = request.args['budget']
-    print(dietes)
     diete = int(dietes[0])
-    resultats = db.avoir_recettes(diete)
+    resultats = db.avoir_recettes(dietes)
     print(resultats)
     return render_template('resultats.html', resultats=resultats)
 
@@ -49,26 +48,29 @@ class Database:
             self.connection = sqlite3.connect('app/db/epicerie.db')
         return self.connection
 
-    def avoir_recettes(self, diete):
+
+    def filtrer_par_diete(self, diete):
         curseur = self.get_connection().cursor()
-        query = (f"""
-                    SELECT r.Nom
-                    FROM Recette r
-                    JOIN Recette_diete rd ON r.Id_Recette = rd.Id_recette
-                    JOIN Diete d ON rd.Id_diète = d.Id_diète
-                    WHERE rd.id_diète = {diete};
-                """)
-        query2 = (f"""
-                    SELECT r.Nom
-                    FROM Recette r
-                    JOIN Recette_diete rd ON r.Id_Recette = rd.Id_recette
-                    JOIN Diete d ON rd.Id_diète = d.Id_diète
-                    WHERE rd.id_diète = {diete};
-                """)
+        query = (
+            f"""
+            SELECT r.Nom
+            FROM Recette r
+            JOIN Recette_diete rd ON r.Id_Recette = rd.Id_recette
+            JOIN Diete d ON rd.Id_diète = d.Id_diète
+            WHERE rd.id_diète = {diete};
+            """
+        )
         curseur.execute(query)
         donnees = curseur.fetchall()
-        curseur.execute(query2)
-        donnees += curseur.fetchall()
-        recettes_set = set(donnees)
-        return [construire_recette(donnee) for donnee in recettes_set]
+        return donnees
 
+
+    def avoir_recettes(self, dietes):
+        donnees = []
+        for diete in dietes:
+            donnees += self.filtrer_par_diete(diete)
+        print(donnees)
+        recettes_set = set(donnees)
+        recettes = [construire_recette(donnee) for donnee in recettes_set]
+        recettes.sort(key=lambda r : r['nom'])
+        return recettes

51ccc712d1de41bdd4ce0be80b3874886dde1bb3
Huard-Lefebvre, Antonin <antoninhuard.lefebvre@gmail.com>
2024-06-18 15:05:23 -0400

    Ajoute tentative de recherche par epicerie

--- app/index.py
+++ app/index.py
@@ -28,16 +28,18 @@ def search():
     allergies = request.args.getlist('allergie')
     dietes = request.args.getlist('diete')
     budget = request.args['budget']
-    diete = int(dietes[0])
-    resultats = db.avoir_recettes(dietes)
-    print(resultats)
+    # diete = int(dietes[0])
+    resultats = db.avoir_recettes(dietes, epiceries)
+    # print(resultats)
     return render_template('resultats.html', resultats=resultats)
+
 
 def construire_recette(donnees):
     recettes = {}
     recettes["nom"] = donnees[0]
 
     return recettes
+
 
 class Database:
     def __init__(self):
@@ -65,12 +67,117 @@ class Database:
         return donnees
 
 
-    def avoir_recettes(self, dietes):
+    def avoir_recettes(self, dietes, epiceries):
         donnees = []
+        # if dietes == []:
+        #     dietes = [0, 1, 2, 3, 4, 5, 6]
         for diete in dietes:
             donnees += self.filtrer_par_diete(diete)
-        print(donnees)
+        self.filtrer_par_epicerie(epiceries)
+        #print(donnees)
         recettes_set = set(donnees)
         recettes = [construire_recette(donnee) for donnee in recettes_set]
         recettes.sort(key=lambda r : r['nom'])
         return recettes
+
+
+    def filtrer_par_epicerie(self, epiceries):
+        # On veut trouver les recettes dont tous les aliments sont disponibles
+        # dans au moins une (on parle d'un OU) des épiceries se trouvant 
+        # dans la liste `epiceries`
+        curseur = self.get_connection().cursor()
+        query = ( 
+            """
+            SELECT DISTINCT aliment_recette.id_recette, 
+                            aliment_recette.id_aliment, 
+                            aliment_epicerie.id_epicerie 
+            FROM aliment_epicerie 
+            JOIN aliment_recette 
+            ON aliment_epicerie.id_aliment = aliment_recette.id_aliment
+            ORDER BY aliment_recette.id_recette
+            """
+        )
+        curseur.execute(query)
+        donnees = curseur.fetchall()
+    
+        recettes = {}
+        recettes_epicerie = {}
+    
+        for (recette_id, aliment_id, epicerie_id) in donnees:
+            aliment = Aliment(aliment_id, epicerie_id)
+            if recette_id not in recettes:
+                recettes[recette_id] = Recette(recette_id)
+            recettes[recette_id].ajouter_aliment(aliment)
+            
+            if str(epicerie_id) in epiceries:
+                if recette_id not in recettes_epicerie:
+                    recettes_epicerie[recette_id] = Recette(recette_id)
+                recettes_epicerie[recette_id].ajouter_aliment(aliment)
+    
+        print(recettes)
+        print("---")
+        print(recettes_epicerie)
+    
+        resultat = []
+        for recette_id, recette in recettes.items():
+            if recette_id in recettes_epicerie:
+                recette_epicerie = recettes_epicerie[recette_id]
+                if recette.ont_memes_aliments(recette_epicerie):
+                    resultat.append(recette)
+        
+        print("2---")
+        print(resultat)
+        return resultat
+
+
+class Aliment():
+    def __init__(self, id, epicerie_id) -> None:
+        self.id = id
+        self.epicerie_id = epicerie_id
+
+    
+    def __eq__(self, other):
+        if isinstance(other, Aliment):
+            return self.id == other.id
+        return False
+
+
+    def __str__(self):
+        return f'(id: {self.id}, epicerie: {self.epicerie_id})'
+    
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+class Recette():
+    def __init__(self, id) -> None:
+        self.id = id
+        self.aliments = set() 
+
+
+    def __eq__(self, other):
+        if isinstance(other, Recette):
+            return self.id == other.id
+        return False
+
+
+    def __str__(self):
+        return f'id: {self.id}, aliments: {self.aliments}'
+
+
+    def __repr__(self) -> str:
+        return str(self) 
+
+
+    def ajouter_aliment(self, aliment: Aliment):
+        self.aliments.add(aliment)
+
+
+    def ont_memes_aliments(self, other):
+        func = lambda x: x.id
+        return (self.aliments == other.aliments)
